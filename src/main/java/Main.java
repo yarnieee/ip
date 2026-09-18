f9b58bd5ed7450cb0f3d6d44e489e4bc7281cf9e
Yi An <yiannie726@gmail.com>
2026-09-18 16:53:43 +0800

    bug fix: main function cannot be found inside jar file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,9 @@ import fella.FartSmella;
 import fella.SmartFella;
 
 public class Main {
-    private AbstractFella fella;
+    private static AbstractFella fella;
 
-    private void instantiateFella() {
+    private static void instantiateFella() {
         int whichFella = (int)(Math.random() * 101) % 2;
 
         switch (whichFella) {
@@ -18,7 +18,7 @@ public class Main {
         }
 
     }
-    public void main() {
+    public static void main(String[] args) {
         instantiateFella();
 
         fella.run();
